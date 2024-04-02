8fd0772270e89bd4c4fbd3e67aee008758aa9bd5
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-02 20:32:21 -0300

    Project Created

---
+++ src/test/java/br/com/ramonsilva186/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ramonsilva186.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
