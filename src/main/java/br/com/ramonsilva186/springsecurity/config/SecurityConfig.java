3184650f44b04e7f1357e42ad13acea9e3649bca
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-03 14:29:13 -0300

    Config spring security for token jwt

---
+++ src/main/java/br/com/ramonsilva186/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package br.com.ramonsilva186.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .csrf(csrl -> csrl.disable())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
