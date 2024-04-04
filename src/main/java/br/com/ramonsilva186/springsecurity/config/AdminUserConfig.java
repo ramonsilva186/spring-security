b0f3ebb4533b7d367d63787f924b9155cb6db79f
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-04 14:44:18 -0300

    function login ready

---
+++ src/main/java/br/com/ramonsilva186/springsecurity/config/AdminUserConfig.java
@@ -0,0 +1,47 @@
+package br.com.ramonsilva186.springsecurity.config;
+
+import br.com.ramonsilva186.springsecurity.entities.Role;
+import br.com.ramonsilva186.springsecurity.entities.User;
+import br.com.ramonsilva186.springsecurity.repository.RoleRepository;
+import br.com.ramonsilva186.springsecurity.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private RoleRepository roleRepository;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("Admin user already exists");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("1905"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+    }
+}
