96e3d9b17ec08f7ac5fe53bb9319165d9ba7c2df
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-05 14:49:39 -0300

    delete tweet no scope

--- src/main/java/br/com/ramonsilva186/springsecurity/controller/TweetController.java
+++ src/main/java/br/com/ramonsilva186/springsecurity/controller/TweetController.java
@@ -4,11 +4,11 @@ import br.com.ramonsilva186.springsecurity.dto.CreateTweetDto;
 import br.com.ramonsilva186.springsecurity.entities.Tweet;
 import br.com.ramonsilva186.springsecurity.repository.TweetRepository;
 import br.com.ramonsilva186.springsecurity.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -33,6 +33,20 @@ public class TweetController {
         tweet.setContent(dto.content());
 
         tweetRepository.save(tweet);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId, JwtAuthenticationToken token) {
+
+        var tweet = tweetRepository.findById(tweetId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(tweetId);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
         return ResponseEntity.ok().build();
     }
 }
